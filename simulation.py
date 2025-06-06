09fe5219235c16aa290569ecdf9c145c493fb459
Lxk3-W <lward248@gmail.com>
2025-06-06 21:57:07 +0100

    .

--- simulation.py
+++ simulation.py
@@ -3,5 +3,8 @@ from mpl_toolkits import mplot3d
 import matplotlib.pypolt as plt
 plt.style.use ('seaborn-poster')
 
-particle_positions = np.random.rand(5, 3)
+particle_pos_x = np.random.rand(5)
+particle_pos_y = np.random.rand(5)
+particle_pos_z = np.random.rand(5)
 
+print(particle_pos_z)
