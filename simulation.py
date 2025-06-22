3c881fb13350b0481f2da35b82b24a22e77459ae
Lxk3-W <lward248@gmail.com>
2025-06-22 21:33:40 +0100

    can now plot points on 2D graph

--- simulation.py
+++ simulation.py
@@ -1,10 +1,17 @@
 import numpy as np
 from mpl_toolkits import mplot3d
-import matplotlib.pypolt as plt
-plt.style.use ('seaborn-poster')
+import matplotlib.pyplot as plt
+import tkinter as tk
+plt.style.use ('seaborn-v0_8-poster')
 
-particle_pos_x = np.random.rand(5)
-particle_pos_y = np.random.rand(5)
-particle_pos_z = np.random.rand(5)
 
-print(particle_pos_z)
+#print(plt.style.available)
+
+part_pos_x = np.random.rand(5)
+part_pos_y = np.random.rand(5)
+part_pos_z = np.random.rand(5)
+
+print(part_pos_z)
+
+plt.scatter(part_pos_x, part_pos_y)
+plt.show()
